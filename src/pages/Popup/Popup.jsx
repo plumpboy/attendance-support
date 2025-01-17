3bfba8ad1e37ada40c84aba60bfbc2c545132cf2
trung <trung@gmail.com>
2025-01-17 18:07:02 +0700

    init

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -10,7 +10,7 @@ import {
 } from '../../api/sra';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import Select from 'react-select'
+import Select from 'react-select';
 
 const Popup = () => {
   const [localStorageData, setLocalStorageData] = useState(null);
@@ -20,6 +20,7 @@ const Popup = () => {
   const [workType, setWorkType] = useState(null);
   const [currentDate, setCurrentDate] = useState(new Date());
   const [workOption, setWorkOption] = useState([]);
+
   const customStyles = {
     control: (styles) => ({
       ...styles,
@@ -37,9 +38,11 @@ const Popup = () => {
       paddingBottom: 0,
     }),
   };
+
   const getCurrentMonth = (date) => {
     return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2);
   };
+
   const [month, setMonth] = useState(getCurrentMonth(currentDate));
   let tabId;
   const setLocalStorage = async () => {
@@ -61,7 +64,7 @@ const Popup = () => {
           return { ...acc, [key]: value };
         }
       },
-      {},
+      {}
     );
     console.log('localStorageData', localStorageData);
     setLocalStorageData(localStorageData);
@@ -73,7 +76,7 @@ const Popup = () => {
     if (localStorageData && localStorageData?.accessToken) {
       chrome.tabs.query(
         { active: true, currentWindow: true },
-        async function(tabs) {
+        async function (tabs) {
           const fromPageLocalStore = await chrome.scripting.executeScript({
             target: { tabId: tabs[0].id },
             function() {
@@ -92,11 +95,11 @@ const Popup = () => {
                 return { ...acc, [key]: value };
               }
             },
-            {},
+            {}
           );
           console.log('localStorageData', localStorageData);
           setLocalStorageData(localStorageData);
-        },
+        }
       );
     }
   }, []);
@@ -104,22 +107,27 @@ const Popup = () => {
   useEffect(() => {
     if (localStorageData?.accessToken) {
       getTypeOfWork(localStorageData.accessToken).then((data) => {
-        const workType = data.typeOfWorks
-        const workTypeOption = workType.map((typeOfWork) => ({ value: typeOfWork.id, label: typeOfWork.name }))
+        const workType = data.typeOfWorks;
+        const workTypeOption = workType.map((typeOfWork) => ({
+          value: typeOfWork.id,
+          label: typeOfWork.name,
+        }));
         // workTypeOption.unshift(value: "", lab)
-        setWorkOption(workTypeOption)
+        setWorkOption(workTypeOption);
         setWorkType('');
       });
-
     }
   }, [localStorageData]);
 
   useEffect(() => {
-    chrome.tabs.query({ currentWindow: true, active: true }, async function(tab) {
-      const isOnSRAPage = tab[0].url.includes('sra.smartosc.com');
-      tabId = tab[0].id;
-      setIsOnSRAPage(isOnSRAPage);
-    });
+    chrome.tabs.query(
+      { currentWindow: true, active: true },
+      async function (tab) {
+        const isOnSRAPage = tab[0].url.includes('sra.smartosc.com');
+        tabId = tab[0].id;
+        setIsOnSRAPage(isOnSRAPage);
+      }
+    );
   }, []);
 
   useEffect(() => {
@@ -128,10 +136,9 @@ const Popup = () => {
     }
   }, [isOnSRAPage]);
 
-
   const goToSRA = async () => {
     await chrome.tabs.update(tabId, { url: 'https://sra.smartosc.com/' });
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       chrome.tabs.onUpdated.addListener(function onUpdated(tabId, info) {
         if (info.status === 'complete') {
           setIsOnSRAPage(true);
@@ -153,34 +160,47 @@ const Popup = () => {
       const date = new Date(`${month}-02T00:00:00`);
       console.log('date', date);
       const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 2);
-      const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 1);
+      const lastDayOfMonth = new Date(
+        date.getFullYear(),
+        date.getMonth() + 1,
+        1
+      );
       const now = new Date();
       console.log('firstDayOfMonth', firstDayOfMonth);
       // If you need the dates in 'yyyy-MM-dd' format
       const startDate = firstDayOfMonth.toISOString().split('T')[0];
-      const endDate = lastDayOfMonth > now ? now.toISOString().split('T')[0]: lastDayOfMonth.toISOString().split('T')[0];
+      const endDate =
+        lastDayOfMonth > now
+          ? now.toISOString().split('T')[0]
+          : lastDayOfMonth.toISOString().split('T')[0];
       console.log('startDate', startDate);
       console.log('endDate', endDate);
       // TODO: Call API get timesheet overview
-      const timesheetOverview = await fetchLogData(localStorageData.accessToken, {
-        username: localStorageData?.userData?.username,
-        startDate,
-        endDate,
-      });
+      const timesheetOverview = await fetchLogData(
+        localStorageData.accessToken,
+        {
+          username: localStorageData?.userData?.username,
+          startDate,
+          endDate,
+        }
+      );
 
       console.log('timesheetOverview', timesheetOverview);
 
       // TODO: Process data and create work logs list
-      const workLogsList = timesheetOverview?.timesheetOverview.filter((log) => {
-        return (
-          !log.isWeekend &&
-          !log.isHoliday &&
-          log.allocated?.totalHours > 0 &&
-          (!log.workLogs || log.workLogs?.totalHours < log.allocated?.totalHours)
-        );
-      });
+      const workLogsList = timesheetOverview?.timesheetOverview.filter(
+        (log) => {
+          return (
+            !log.isWeekend &&
+            !log.isHoliday &&
+            log.allocated?.totalHours > 0 &&
+            (!log.workLogs ||
+              log.workLogs?.totalHours < log.allocated?.totalHours)
+          );
+        }
+      );
       console.log('workLogsList', workLogsList);
-      if(workLogsList.length > 0) {
+      if (workLogsList.length > 0) {
         for (let i = 0; i < workLogsList.length; i++) {
           const res = await getProjects(localStorageData.accessToken, {
             userId: localStorageData?.userData?.id,
@@ -188,35 +208,42 @@ const Popup = () => {
           });
           const allowcatedData = workLogsList?.[i]?.allocated?.detail;
           for (let j = 0; j < allowcatedData?.length; j++) {
-            const logHour = workLogsList?.[i].workLogs ? Object.values(workLogsList?.[i]?.workLogs?.detail).find((data) => data.code === allowcatedData[j].code)?.hours : 0;
+            const logHour = workLogsList?.[i].workLogs
+              ? Object.values(workLogsList?.[i]?.workLogs?.detail).find(
+                  (data) => data.code === allowcatedData[j].code
+                )?.hours
+              : 0;
 
-            let workLogsData = [{
-              date: workLogsList[i].date,
-              description: null,
-              workHours:  logHour ? allowcatedData[j]?.hours - logHour : allowcatedData[j]?.hours,
-              typeOfWork: workType,
-              projectId: res.find((project) => project.code === allowcatedData[j].code)?.id,
-            }];
-            console.log('workLogsData', workLogsData)
-            if(workLogsData[0].workHours > 0) {
+            let workLogsData = [
+              {
+                date: workLogsList[i].date,
+                description: null,
+                workHours: logHour
+                  ? allowcatedData[j]?.hours - logHour
+                  : allowcatedData[j]?.hours,
+                typeOfWork: workType,
+                projectId: res.find(
+                  (project) => project.code === allowcatedData[j].code
+                )?.id,
+              },
+            ];
+            console.log('workLogsData', workLogsData);
+            if (workLogsData[0].workHours > 0) {
               await postWorkLogs(localStorageData.accessToken, {
                 workLogs: workLogsData,
-              })
+              });
             }
-
           }
-
         }
       }
 
       setSuccessMessage('Log work successful!');
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.update(tabs[0].id, { url: tabs[0].url });
       });
     } catch (e) {
       setErrorMessage(e.message);
     }
-
   };
 
   const handleWorkTypeChange = (event) => {
@@ -237,55 +264,61 @@ const Popup = () => {
       <header className="App-header">
         <div class="title">
           <img src={logo} className="logo" alt="logo" />
-          <p class="text-title">SRA++</p>
+          <p class="text-title">Zoho Attendance Support</p>
         </div>
-        {
-          isOnSRAPage ?
-            <div class="container">
-              <div class="date-input">
-                <label htmlFor="month">Month: </label>
-                <br />
-                <div class="date-picker">
-                  <DatePicker selected={currentDate}
-                              onChange={handleMonthChange}
-                              showMonthYearPicker={true}
-                              dateFormat="yyyy-MM"
-                              maxDate={new Date()}
-                  />
-                </div>
-              </div>
-              <div class="option-input">
-                <label htmlFor="workType">Type of Work: </label>
-                <div class="select-option">
-                  <Select options={workOption} onChange={handleWorkTypeChange} styles={customStyles}  className="react-select-container" placeholder="Select work type"/>
-                </div>
-              </div>
-              <button className="log-button" type="button" onClick={handleClick}>
-                Log work
-              </button>
-            </div>
-            :
-            <div class="go-to-sra">
-              <button className="log-button" type="button" onClick={goToSRA}>Go to SRA</button>
+        {isOnSRAPage ? (
+          <div class="container">
+            <div class="date-input">
+              <label htmlFor="month">Month: </label>
               <br />
-              <p> You need go to SRA to log work</p>
+              <div class="date-picker">
+                <DatePicker
+                  selected={currentDate}
+                  onChange={handleMonthChange}
+                  showMonthYearPicker={true}
+                  dateFormat="yyyy-MM"
+                  maxDate={new Date()}
+                />
+              </div>
             </div>
-        }
-        {
-          errorMessage !== '' &&
+            <div class="option-input">
+              <label htmlFor="workType">Type of Work: </label>
+              <div class="select-option">
+                <Select
+                  options={workOption}
+                  onChange={handleWorkTypeChange}
+                  styles={customStyles}
+                  className="react-select-container"
+                  placeholder="Select work type"
+                />
+              </div>
+            </div>
+            <button className="log-button" type="button" onClick={handleClick}>
+              Log work
+            </button>
+          </div>
+        ) : (
+          <div class="go-to-sra">
+            <button className="log-button" type="button" onClick={goToSRA}>
+              Go to SRA
+            </button>
+            <br />
+            <p> You need go to SRA to log work</p>
+          </div>
+        )}
+        {errorMessage !== '' && (
           <div class="error-message">
             <br />
             {errorMessage}
           </div>
-        }
+        )}
 
-        {
-          successMessage !== '' &&
+        {successMessage !== '' && (
           <div class="suceess-message">
             <br />
             {successMessage}
           </div>
-        }
+        )}
       </header>
     </div>
   );
--- todo.md
+++ todo.md
@@ -1,6 +1,25 @@
-1. July Todo List
-- log replace (delete and create new) (option) []
-- log only avaiable hours []
-- log per project []
-- log max to current date []
-- change css []
+features:
++ today working time
+  - estimate checkout time to fullfill 6h and 8h
+  - show current day working time
+
++ monthy report
+  - calculate late arrival days (8 > x > =6)
+  - calculate wfh days ( < 6h)
+
++ utils:
+  - auto create attendance requests
+  - auto create leaves
+
+  priority:
+    1.attendance requests: non-checkin > below 6 > below 8
+    2.leaves: non-checkin < below 6 < below 8
+
+    if none of requests remain, use leave
+
+  flow: show popup contain request list then user can approve request or skip.
+
++ ui (content script)
+
+  - create button to show pop up, a badge to show remaining requests(attendance requests and latable days), current late arrival days and wfh days
+
